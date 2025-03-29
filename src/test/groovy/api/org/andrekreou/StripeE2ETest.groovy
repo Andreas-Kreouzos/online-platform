daf96a2508b76ca2da894e3c7f4d43bd839819b9
Andreas Kreouzos <andreas.kreouzos@hotmail.com>
2025-03-29 14:40:30 +0200

    Applied end2end test with Auth Code PKCE Flow, added Keycloak DEV Services

--- src/test/groovy/api/org/andrekreou/StripeE2ETest.groovy
+++ src/test/groovy/api/org/andrekreou/StripeE2ETest.groovy
@@ -1,34 +1,64 @@
 package api.org.andrekreou
 
+import groovy.json.JsonSlurper
 import io.quarkus.test.junit.QuarkusTest
-import io.quarkus.test.security.TestSecurity
-import jakarta.ws.rs.core.MediaType
+import org.eclipse.microprofile.config.inject.ConfigProperty
+import org.htmlunit.WebClient
+import org.htmlunit.html.HtmlForm
+import org.htmlunit.html.HtmlPage
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
-import static io.restassured.RestAssured.given
-import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals
 
 @QuarkusTest
 class StripeE2ETest {
 
+    WebClient webClient
+
+    @ConfigProperty(name = 'stripe.admin.username')
+    String username
+
+    @ConfigProperty(name = 'stripe.admin.password')
+    String password
+
+    @BeforeEach
+    void setup() {
+        webClient = new WebClient()
+        webClient.getOptions().setCssEnabled(false)
+        webClient.getOptions().setRedirectEnabled(true)
+        webClient.getCookieManager().setCookiesEnabled(true)
+    }
+
     @Test
-    @TestSecurity(user = "andrekreou", roles = "admin")
     @DisplayName("Successfully call retrieve transaction")
     void testSuccessfullyRetrieveTransaction() {
-        String id = 'txn_3R2xAKFZLv1HNLFS1SQXtbmq'
+        //given: a valid transaction ID
+        String transactionId = 'txn_3R2xAKFZLv1HNLFS1SQXtbmq'
 
-        given()
-                .contentType(MediaType.APPLICATION_JSON)
-                .queryParam('id', id)
-                .when()
-                .get('/stripe/transaction')
-                .then()
-                .log().all()
-                .statusCode(200)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body("id", equalTo(id))
-                .body("currency", equalTo('eur'))
-                .body("status", equalTo('available'))
+        //when: calling the resource
+        HtmlPage loginPage = webClient.getPage("http://localhost:8080/stripe/transaction?id=${transactionId}")
+
+        //and: the login form of Keycloak gets loaded
+        HtmlForm loginForm = loginPage.getForms().get(0)
+
+        //and: providing the appropriate user credentials
+        loginForm.getInputByName("username").type(username)
+        loginForm.getInputByName("password").type(password)
+
+        //and: submitting the form
+        def loginResponse = loginForm.getButtonByName("login").click()
+
+        //then: the endpoint response gets returned
+        String response = loginResponse.getWebResponse().getContentAsString()
+
+        //and: parse the response
+        def parsedJson = new JsonSlurper().parseText(response)
+
+        //and: the expected field values are matched with the actual ones
+        assertEquals(transactionId, parsedJson.id)
+        assertEquals('eur', parsedJson.currency)
+        assertEquals('available', parsedJson.status)
     }
 }
